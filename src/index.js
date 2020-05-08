1c0ba8cd22e6f1a08a3076a2c9c7b4d05388689c
jiakewei <jiakewei@doushen.com>
2020-05-08 18:14:16 +0800

    测试一下

--- src/index.js
+++ src/index.js
@@ -9,3 +9,4 @@ ReactDOM.render(<TodoList />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+console.log('123')
