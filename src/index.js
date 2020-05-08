0d4449cf9ca310690028a812b7729db234d33fe2
jiakewei <jiakewei@doushen.com>
2020-05-08 19:01:27 +0800

    分支会更新

--- src/index.js
+++ src/index.js
@@ -9,10 +9,4 @@ ReactDOM.render(<TodoList />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-console.log('123')
-console.log('34567')
-console.log('123456789')
-console.log('test分支更新')
-console.log('master检出分支')
-console.log('master检出分支本地修改')
-console.log('master更新了')
+console.log('分支从头再来')
